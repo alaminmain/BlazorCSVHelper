a2de52ac68f98353c02fd657f9a68d0fdc4207cb
Al-Amin Hossain <alaminmain@gmail.com>
2025-01-08 15:12:15 +0600

    Add project files.

---
+++ CSVAPI/Entities/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSVAPI.Entities
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
